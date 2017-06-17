a0c0407d373dc6df826b0173c2fa524af9a46aea
num46664 <blwakila@gmail.com>
2017-06-17 11:52:15 -0700

    first push

--- scrapers/poemhunter.py
+++ scrapers/poemhunter.py
@@ -5,14 +5,22 @@ import re, time, os
 
 SLEEP_TIME_S = 10
 
+def get_page(url,timeout):
+	while(True):
+		try:
+			return urlreq.urlopen(url)
+			break
+		except urlerr.HTTPError:
+			time.sleep(timeout)
+			trycount += 1
+		print("try " + str(trycount))
+
 def get_poems(url):
     poems = []
 	trycount = 0
-	while(True):
-		try:
-			page = urlreq.urlopen(url)
-			break
-		except urlerr.HTTPError:
-			time.sleep(SLEEP_TIME_S)
-			trycount += 1
-		print("try " + str(trycount))
+    page = get_page(url,SLEEP_TIME_S)
+    soup = BeautifulSoup(page,"html.parser")
+    kona_div = soup.find_all("div",class_ = "KonaBody")
+    if kona_div:
+        poem = kona_div[0].find('p').text
+    
